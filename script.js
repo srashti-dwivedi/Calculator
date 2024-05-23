3bc03cfe6b3efbf66f67b96ba26b23453447f947
srashti-dwivedi <srashtidwivedi4@gmail.com>
2024-05-23 11:10:47 +0530

    javascript added

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var string =""
+const buttons =document.querySelectorAll("button")
+ Array.from(buttons).forEach((val)=>{
+    val.addEventListener("click",(e)=>{
+      if (e.target.innerHTML=='='){
+        string=eval(string);
+        document.querySelector("input").value=string
+      }
+      else if(e.target.innerHTML=='C'){
+        string=""
+        document.querySelector("input").value=string
+
+      }
+      else if(e.target.innerHTML=='Del'){
+        
+        string=string.substring(0,string.length-1)
+        document.querySelector("input").value=string
+
+      }
+    
+      else{
+        string=string+e.target.innerHTML
+        document.querySelector("input").value=string
+      }
+    })
+})
